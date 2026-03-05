f33bbc4f189a19e5999353949ffa3125a35660a6
marvan-srv <sayyid@srvithub.com>
2026-03-05 23:21:15 +0530

    added new exp and resume

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -38,24 +38,20 @@ const Qualification = () => {
         </div>
 
         <div className="qualification__sections">
-          {/* Education Section */}
+          {/* ---------- Education Section ---------- */}
           <div
             className={
               toggle === 1
                 ? "qualification__content qualification__content-active"
-                : "qualification__content "
+                : "qualification__content"
             }
           >
             <div className="qualification__data">
               <div>
-                <h3 className="qualification__title">
-                B.Tech in CSE.
-                </h3>
-                <span className="qualification__subtitle">
-                  VTU -Bangalore
-                </span>
+                <h3 className="qualification__title">B.Tech in CSE</h3>
+                <span className="qualification__subtitle">VTU – Bangalore</span>
                 <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i> 2019 - 2023
+                  <i className="uil uil-calendar-alt"></i> 2019 – 2023
                 </div>
               </div>
               <div>
@@ -72,11 +68,9 @@ const Qualification = () => {
               </div>
               <div>
                 <h3 className="qualification__title">Higher Secondary</h3>
-                <span className="qualification__subtitle">
-                  Kerala
-                </span>
+                <span className="qualification__subtitle">Kerala Board</span>
                 <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i> 2017 - 2019
+                  <i className="uil uil-calendar-alt"></i> 2017 – 2019
                 </div>
               </div>
             </div>
@@ -84,11 +78,35 @@ const Qualification = () => {
             <div className="qualification__data">
               <div>
                 <h3 className="qualification__title">SSLC</h3>
+                <span className="qualification__subtitle">AKM – Kerala</span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> 2016 – 2017
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+          </div>
+
+          {/* ---------- Experience Section ---------- */}
+          <div
+            className={
+              toggle === 2
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            {/* SRV InfoTech – left */}
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">MERN Stack Developer</h3>
                 <span className="qualification__subtitle">
-                  AKM - Kerala
+                  SRV InfoTech – Kannur
                 </span>
                 <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i> 2016 - 2017
+                  <i className="uil uil-calendar-alt"></i> Jan 2026 – Present
                 </div>
               </div>
               <div>
@@ -97,39 +115,32 @@ const Qualification = () => {
               </div>
             </div>
 
-            {/* <div className="qualification__data">
+            {/* Webeaz Technologies – right */}
+            <div className="qualification__data">
               <div></div>
               <div>
                 <span className="qualification__rounder"></span>
                 <span className="qualification__line"></span>
               </div>
               <div>
-                <h3 className="qualification__title">Art Director</h3>
+                <h3 className="qualification__title">Software Trainee</h3>
                 <span className="qualification__subtitle">
-                  Spanin - Institute
+                  Webeaz Technologies – Kozhikode
                 </span>
                 <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                  <i className="uil uil-calendar-alt"></i> Nov 2025 – Dec 2025
                 </div>
               </div>
-            </div> */}
-          </div>
+            </div>
 
-          {/* Experience Section */}
-          <div
-            className={
-              toggle === 2
-                ? "qualification__content qualification__content-active"
-                : "qualification__content "
-            }
-          >
+            {/* Mentorow – left */}
             <div className="qualification__data">
               <div>
                 <h3 className="qualification__title">
                   Full Stack Developer Intern
                 </h3>
                 <span className="qualification__subtitle">
-                  CipherByte Technologies - Remote
+                  CipherByte Technologies – Remote
                 </span>
                 <div className="qualification__calendar">
                   <i className="uil uil-calendar-alt"></i> Jul 2024 – Aug 2024
@@ -141,6 +152,7 @@ const Qualification = () => {
               </div>
             </div>
 
+            {/* CipherByte Technologies – right */}
             <div className="qualification__data">
               <div></div>
               <div>
@@ -148,50 +160,15 @@ const Qualification = () => {
                 <span className="qualification__line"></span>
               </div>
               <div>
-                <h3 className="qualification__title">
-                  MERN Stack Developer Intern
-                </h3>
+                <h3 className="qualification__title">MERN Stack Trainee</h3>
                 <span className="qualification__subtitle">
-                  Mentorow Technology - Kochi
+                  Mentorow – Kochi
                 </span>
                 <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i> Oct 2023 – Jul 2024
+                  <i className="uil uil-calendar-alt">Oct 2023 – Jun 2024</i>
                 </div>
               </div>
             </div>
-
-            {/* <div className="qualification__data">
-              <div>
-                <h3 className="qualification__title">Web Design</h3>
-                <span className="qualification__subtitle">
-                  Spanin - Institute
-                </span>
-                <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i> 2021 - Present
-                </div>
-              </div>
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
-              </div>
-            </div> */}
-
-            {/* <div className="qualification__data">
-              <div></div>
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
-              </div>
-              <div>
-                <h3 className="qualification__title">Art Director</h3>
-                <span className="qualification__subtitle">
-                  Spanin - Institute
-                </span>
-                <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i> 2021 - Present
-                </div>
-              </div>
-            </div> */}
           </div>
         </div>
       </div>
